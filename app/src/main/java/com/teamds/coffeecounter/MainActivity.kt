3264c205a1f13030267e12195008dca39ece20ce
KG <gundaegi2@gmail.com>
2020-05-15 00:51:26 +0900

    Initial commit

---
+++ app/src/main/java/com/teamds/coffeecounter/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.teamds.coffeecounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+
+        bottomNavigationView.setupWithNavController(navController)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+    }
+}
