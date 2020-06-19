0894b3227fab5ff89a5335b7df0436726ce85182
RollinKG <gundaegi2@gmail.com>
2020-06-19 15:57:03 +0900

    20200619

--- app/src/main/java/com/teamds/coffeecounter/main/HomeFragment.kt
+++ app/src/main/java/com/teamds/coffeecounter/main/HomeFragment.kt
@@ -5,19 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.teamds.coffeecounter.HomeStatus
 import com.teamds.coffeecounter.R
+import com.teamds.coffeecounter.databinding.FragmentHomeBinding
+import com.teamds.coffeecounter.stat.StatCoffeePieFragment
+import com.teamds.coffeecounter.stat.StatDayPieFragment
+import com.teamds.coffeecounter.stat.StatWeeklyFragment
 
 /**
  * A simple [Fragment] subclass.
  */
 class HomeFragment : Fragment() {
 
+    lateinit var binding : FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        val adapter = ViewPagerAdapter(this.requireActivity())
+        binding.homeViewpager.adapter = adapter
+        binding.homeViewpager.isUserInputEnabled=false
+
+
+        return binding.root
+    }
+
+    private inner class ViewPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa){
+        override fun createFragment(position: Int): Fragment {
+            return when(position){
+                0 -> HomeStatus()
+                else -> HomeStatus()
+            }
+        }
+        override fun getItemCount(): Int = 1
     }
 
 }
