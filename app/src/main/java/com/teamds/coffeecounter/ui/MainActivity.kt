07e088b78582a2102cf751b92879ec20df3d886c
RollinKG <gundaegi2@gmail.com>
2021-05-08 08:17:59 +0900

    feat: 메인화면 Fragment Navigation 추가
    - Bottom Bar와 Navigation 연결

--- app/src/main/java/com/teamds/coffeecounter/ui/MainActivity.kt
+++ app/src/main/java/com/teamds/coffeecounter/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.teamds.coffeecounter.ui
 
 import android.os.Bundle
+import android.view.Menu
+import androidx.navigation.findNavController
 import com.teamds.coffeecounter.BaseActivity
 import com.teamds.coffeecounter.R
 import com.teamds.coffeecounter.databinding.ActivityMainBinding
@@ -12,6 +14,15 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main){
         super.onCreate(savedInstanceState)
         binding.apply {
 
+
         }
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.hide()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        binding.bottomBar.setupWithNavController(menu!!, navController = findNavController(R.id.nav_host_fragment))
+        return true
     }
 }
