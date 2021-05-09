f23caaf19119e61c40f6aed354f8407825224910
RollinKG <gundaegi2@gmail.com>
2021-05-09 10:00:17 +0900

    feat: CameraX 라이브러리 추가
    - CameraX 의존성 추가
    - Camera 관련 Permission 추가
    - 기본 Preview Layout 추가

--- app/src/main/java/com/teamds/coffeecounter/ui/CameraFragment.kt
+++ app/src/main/java/com/teamds/coffeecounter/ui/CameraFragment.kt
@@ -1,29 +1,85 @@
 package com.teamds.coffeecounter.ui
 
-import androidx.lifecycle.ViewModelProvider
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.camera.core.ImageCapture
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.teamds.coffeecounter.BaseFragment
 import com.teamds.coffeecounter.R
 import com.teamds.coffeecounter.databinding.CameraFragmentBinding
 import com.teamds.coffeecounter.viewmodel.CameraViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraFragment : BaseFragment<CameraFragmentBinding>(R.layout.camera_fragment) {
 
     private val viewModel: CameraViewModel by viewModel()
+    private var imageCapture: ImageCapture? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        // Set up the listener for take photo button
+        binding.btnCapture.setOnClickListener { takePhoto() }
+
+        outputDirectory = getOutputDirectory()
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
 
     }
 
+    private fun takePhoto() {}
+
+    private fun startCamera() {}
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireActivity().baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = requireActivity().externalMediaDirs.firstOrNull().let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir.exists())
+            mediaDir else requireActivity().filesDir
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
 }
