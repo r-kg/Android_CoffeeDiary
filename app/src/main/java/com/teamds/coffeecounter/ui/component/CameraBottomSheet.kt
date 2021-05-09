7b9c081b8de009089450682bd701d0cde8b0103b
RollinKG <gundaegi2@gmail.com>
2021-05-09 13:46:37 +0900

    feat: Camera BottomSheet UI 추가
    - AutoComplete TextView, botton, style 적용

--- app/src/main/java/com/teamds/coffeecounter/ui/component/CameraBottomSheet.kt
+++ app/src/main/java/com/teamds/coffeecounter/ui/component/CameraBottomSheet.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.teamds.coffeecounter.R
 import com.teamds.coffeecounter.databinding.CameraBottomSheetBinding
@@ -12,6 +13,11 @@ import com.teamds.coffeecounter.databinding.CameraBottomSheetBinding
 class CameraBottomSheet : BottomSheetDialogFragment() {
 
     lateinit var binding : CameraBottomSheetBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.AppBottomSheetDialogTheme)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
